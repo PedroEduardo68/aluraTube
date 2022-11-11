568fc87895df4350735685a9b575b25d0fc1b850
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-11-10 23:22:29 -0300

    up hook in react js aluratube

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -2,11 +2,34 @@ import { useState } from "react";
 import { StyledRegisterVideo } from "./styles"
 
 
+const useFrom = (Props) =>{
+    const [values,setValues] = useState(Props.initialvalues)
+
+    const handleChange = (event) =>{ 
+        const value = event.target.value;
+        const name = event.target.name;
+        setValues({...values, [name] :value })
+    }
+
+    const clearform = () =>{
+        setValues({})
+    }
+
+    return {
+        values,
+        handleChange,
+        clearform
+    }
+}
+
 
 const RegisterVideo = () =>{
+    const formCadastro = useFrom({
+        initialvalues : {titulo : "", url: ""}
+    })
     const [formVisivel,setFormVisivel] = useState(false)
-    const [titulo,setTitulo] = useState("")
-    const [url,setUrl] = useState('')
+   
+
     
     return (
         <>
@@ -15,13 +38,17 @@ const RegisterVideo = () =>{
                     +
                 </button>
                 {formVisivel && 
-                                <form>
+                            <form onSubmit={(event) =>{
+                                event.prventDefault();
+                                setFormVisivel(false)
+                                formCadastro.clearform()
+                            }}>
                                 <div>
-                                    <button className="close-modal" onClick={() => setFormVisivel(false)}>
+                                    <button type="button" className="close-modal" onClick={() => setFormVisivel(false)}>
                                         X
                                     </button>
-                                    <input placeholder="Titulo do Vídeo" />
-                                    <input placeholder="URL"/>
+                                    <input placeholder="Titulo do Vídeo" values={formCadastro.values.titulo} onChange={formCadastro.handleChange} name="titulo" />
+                                    <input placeholder="URL" values={formCadastro.values.url} onChange={formCadastro.handleChange}   name="url" />
                                     <button type="submit">
                                         Cadastrar
                                     </button>
