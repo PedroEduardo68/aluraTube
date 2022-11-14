305f2e578b35c330efefad4c7657c0177e9c00ab
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-11-14 00:58:54 -0300

    finaly update

--- src/components/services/videoService.js
+++ src/components/services/videoService.js
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJECT_URL = "https://okstarojudwxmjdcuhea.supabase.co";
-const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rc3Rhcm9qdWR3eG1qZGN1aGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4OTU1OTUsImV4cCI6MTk4MDQ3MTU5NX0.shktap50ImF9SqhqcsugSpg38trqKe7aNEP4RvhpQE0";
-const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
+
+const supabaseUrl = 'https://guroxesxctmenafhhudh.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1cm94ZXN4Y3RtZW5hZmhodWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgzOTM2NjgsImV4cCI6MTk4Mzk2OTY2OH0.SjPLtP75hOeRnZtXCeaZ9MJrMQX-QowuGMAvu003nWs'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export function videoService() {
     return {
